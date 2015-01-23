5ea0a8c0f8f6cc2401c8a459e46a1acb8c2b4b31
oli <olizehm@gmail.com>
2015-01-23 15:10:09 +0100

    test

--- login.php
+++ login.php
@@ -20,7 +20,6 @@
                     /:::/    /               /:::/    /              \:::\____\       \:::\____\        \::/____/
                     \::/    /                \::/    /                \::/    /        \::/    /         ~~
                      \/____/                  \/____/                  \/____/          \/____/
-test
 -->
 <head>
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"> <!-- Bootstrap CSS laden -->
