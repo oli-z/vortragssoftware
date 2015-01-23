105220d91b7bae9ac2c246819dde05ad6a1f5099
oli <olizehm@gmail.com>
2015-01-23 14:25:10 +0100

    Config-Datei hinzugefügt

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// MySQL-Einstellungen
+$db_server = "" // DB-Server
+$db_user = "" // DB-Nutzername
+$db_pass = "" // DB-Passwort
+$db_name = "" // DB-Name
+
+// allgemeine Einstellungen
+$lang = "de" // Sprache - Sprachdatei muss im "lang"-Ordner liegen
+?>
