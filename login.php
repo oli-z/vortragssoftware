4f9e48bbd0b83c3bde0301cdaca0b9a5bbe4d04d
oli <olizehm@gmail.com>
2015-01-23 14:53:18 +0100

    Einrückungen verbessert

--- login.php
+++ login.php
@@ -1,37 +1,36 @@
 <head>
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"> <!-- Bootstrap CSS laden -->
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"> <!-- Bootstrap CSS laden -->
 </head>
 <body>
-<?php
+  <?php
 
-require_once('auth.php');
-require_once('config.php');
-require_once('lang/'.$lang.'.php');
+  require_once('auth.php');
+  require_once('config.php');
+  require_once('lang/'.$lang.'.php');
 
-$logincomplete=false;
-if ($_POST["pass"]&&$_POST["user"]&&$_POST["sub"]) {
-$result = auth::logon($_POST["user"],hash("sha512",$_POST["pass"]));
+  $logincomplete=false;
+  if ($_POST["pass"]&&$_POST["user"]&&$_POST["sub"]) {
+    $result = auth::logon($_POST["user"],hash("sha512",$_POST["pass"]));
 
-if ($result==true) {
-$chash=auth::createRandomKey();
-$duration=time()+600;
-mysql_connect("localhost","root") or die ('<div class="alert alert-danger" role="alert">cant connect to SQL</div>');
-mysql_query('use schuletest') or die ('<div class="container theme-showcase" role="main"><div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-exclamation-sign"></span>&emsp;'.$sqldberror.'</div></div>');
-$insert=mysql_query('insert into session (cid,void) values ("'.$chash.'",'.$duration.')') or die ('<div class="alert alert-danger" role="alert">cant insert</div>');
-mysql_close();
-setcookie('key',$chash,$duration);
-$logincomplete=true;
-}
-else
-setcookie('key','lol',1);
-}
-if($_POST["kill"])
-{
-mysql_connect("localhost","root") or die ("cant connect to SQL");
-mysql_query('use schuletest') or die ('<div class="container theme-showcase" role="main"><div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-exclamation-sign"></span>&emsp;'.$sqldberror.'</div></div>');
-mysql_query('delete from session where cid like "'.$_COOKIE["key"].'"') or die ('cant delete');
-mysql_close();
-}
+    if ($result==true) {
+      $chash=auth::createRandomKey();
+      $duration=time()+600;
+      mysql_connect("localhost","root") or die ('<div class="alert alert-danger" role="alert">cant connect to SQL</div>');
+      mysql_query('use schuletest') or die ('<div class="container theme-showcase" role="main"><div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-exclamation-sign"></span>&emsp;'.$sqldberror.'</div></div>');
+      $insert=mysql_query('insert into session (cid,void) values ("'.$chash.'",'.$duration.')') or die ('<div class="alert alert-danger" role="alert">cant insert</div>');
+      mysql_close();
+      setcookie('key',$chash,$duration);
+      $logincomplete=true;
+    }
+    else
+    setcookie('key','lol',1);
+  }
+  if($_POST["kill"]) {
+    mysql_connect("localhost","root") or die ("cant connect to SQL");
+    mysql_query('use schuletest') or die ('<div class="container theme-showcase" role="main"><div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-exclamation-sign"></span>&emsp;'.$sqldberror.'</div></div>');
+    mysql_query('delete from session where cid like "'.$_COOKIE["key"].'"') or die ('cant delete');
+    mysql_close();
+  }
 ?>
 
 
