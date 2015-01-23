6d704299d0c6ce89b91f5128fdc5e460b0d94102
oli <olizehm@gmail.com>
2015-01-23 14:04:17 +0100

    initial commit

---
+++ lang.php
@@ -0,0 +1,4 @@
+<?php
+$sqldberror="<strong>Fehler:</strong> Es konnte keine Verbindung zur MySQL-Datenbank hergestellt werden.";
+$sqlservererror="<strong>Fehler:</strong> es konnte keine Verbindung zum MySQL-Server hergestellt werden."
+?>
