3bba1b47b725d228d7104611257ea9df616c192a
My1 <teamhydro55555@gmail.com>
2015-02-09 22:45:32 +0100

    Update auth.php
    Cookieumgang und Login/Logout aus login.php übernommen
    
    Überdimensionierte sicherheit (AES Engine, noch unbenutzt -> für multiuser, IP Check [gehasht, implementiert, muss noch getestet werden])
    WICHTIG (egal): neuer Keystyle in DB
    SHA-512 von 256 Zeichen langem Cookie-Key (128 Zeichen) plus SHA-512 von IP (128 Zeichen)
    
    WICHTIG (bei Usererstellung: Neuer PW-Style in DB
    SHA-512->Doppelter SHA-512 (erstellten Hash nochmal hashen) -> Rainbow Table Schutz
    '-> Alte passwörter durch SHA-512 jagen für kompatibilität -> kein weg zurück

--- auth.php
+++ auth.php
@@ -7,6 +7,21 @@ global $sqldberror;
 
 
 class auth {
+//AES descrypt
+function aescrypt($encrypt, $mc_key) {
+    $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB),MCRYPT_RAND);
+    $passcrypt = trim(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $mc_key,trim($encrypt),MCRYPT_MODE_ECB, $iv));
+    $encode = base64_encode($passcrypt);
+    return $encode;
+}
+
+	
+function aesdecrypt($decrypt, $mc_key) {   
+    $decoded = base64_decode($decrypt);
+    $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB),MCRYPT_RAND);
+    $decrypted = trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $mc_key, trim($decoded),MCRYPT_MODE_ECB, $iv));
+    return $decrypted;
+}
   
 //remove special chars -> SQL Injection
 public static function clean($z){
@@ -15,7 +30,16 @@ public static function clean($z){
     $z = str_replace(' ', '', $z);
 }
 
-
+  public static function ip(){
+    if (! isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+      $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    else {
+      $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    return $ip
+  }
+  
   public static function verify() {
     // open ckey database
     global $sqldberror;
@@ -23,13 +47,15 @@ public static function clean($z){
 
     mysql_query('use schuletest') or die ('<div class="container theme-showcase" role="main"><div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-exclamation-sign"></span>&emsp;'.$sqldberror.'(verify)</div></div>');
 
-
+    //get IP
+    $ip=ip();
     //delete old cIDs
     $del=mysql_query('delete from session where void<'.time()) or die ("can delete old cIDs (verify)");
     //clean key
     $key=clean($_COOKIE["key"]);
+    $key=hash("sha512",$_COOKIE["key"].hash("sha512",$ip);
     //check key
-    if(strlen($key)>=256){
+    if(strlen($key)=256){
       $check=mysql_query('select count(cID) from session where cid like "'.$key.'"');
       $num = mysql_result ($check,0);
       if ($num>0){
@@ -57,6 +83,13 @@ public static function clean($z){
     return $randkey;
   }
 
+  public static funtion logout() {
+    mysql_connect("localhost","root") or die ("cant connect to SQL");
+    mysql_query('use schuletest') or die ('<div class="container theme-showcase" role="main"><div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-exclamation-sign"></span>&emsp;'.$sqldberror.' (logout)</div></div>');
+    mysql_query('delete from session where cid like "'.hash("sha512",$_COOKIE["key"].hash("sha512",$ip).'"') or die ('cant delete');
+    mysql_close();
+  }
+
   public static function logon($user, $pw) {
     global $sqldberror;
     // open ckey database
@@ -70,12 +103,21 @@ public static function clean($z){
     $loginfo=mysql_query('select password from users where uname like "'.$user.'"') or die ("Datenbankproblem oder user existiert nicht, bitte zurück (logon)");
     $loginfo=mysql_fetch_row($loginfo)[0];
     mysql_close();
-    if($pw==$loginfo)
+    if(hash("sha512",hash("sha512",$pw))==$loginfo)
     {
+      $chash=auth::createRandomKey();
+      $duration=time()+600;
+      mysql_connect("localhost","root") or die ('<div class="alert alert-danger" role="alert">cant connect to SQL</div>');
+      mysql_query('use schuletest') or die ('<div class="container theme-showcase" role="main"><div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-exclamation-sign"></span>&emsp;'.$sqldberror.'</div></div>');
+      $insert=mysql_query('insert into session (cid,void) values ("'.hash("sha512",$chash.hash("sha512",$ip).'",'.$duration.')') or die ('<div class="alert alert-danger" role="alert">cant insert</div>');
+      mysql_close();
+      setcookie('key',$chash,$duration);
       return true;
     }
-    else
-    return false;
+    else{
+      setcookie('key','lol',1);
+      return false;
+    }
   }
 }
 ?>
