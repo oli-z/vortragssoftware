e20e63b390c80f25dda8560fb79c0f9a0b64f190
My1 <teamhydro55555@gmail.com>
2015-02-09 21:05:12 +0100

    Update auth.php
    Reinigungsfunktion hinzugefügt (Sonderzeichen entfernen)
    Fehlermeldungen verbessert
    Besserer check von defekten bzw Manipulierten Session-Keys

--- auth.php
+++ auth.php
@@ -7,30 +7,44 @@ global $sqldberror;
 
 
 class auth {
+  
+//remove special chars -> SQL Injection
+public static function clean($z){
+    $z = strtolower($z);
+    $z = preg_replace('/[^a-zA-Z0-9]+/', '', $z);
+    $z = str_replace(' ', '', $z);
+}
+
+
   public static function verify() {
     // open ckey database
     global $sqldberror;
-    mysql_connect('localhost',"root") or die ('<div class="alert alert-danger" role="alert">cant connect to SQL</div>');
+    mysql_connect('localhost',"root") or die ('<div class="alert alert-danger" role="alert">cant connect to SQL (verify)</div>');
 
-    mysql_query('use schuletest') or die ('<div class="container theme-showcase" role="main"><div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-exclamation-sign"></span>&emsp;'.$sqldberror.'</div></div>');
+    mysql_query('use schuletest') or die ('<div class="container theme-showcase" role="main"><div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-exclamation-sign"></span>&emsp;'.$sqldberror.'(verify)</div></div>');
 
 
     //delete old cIDs
-    $del=mysql_query('delete from session where void<'.time()) or die ("can delete old cIDs");
+    $del=mysql_query('delete from session where void<'.time()) or die ("can delete old cIDs (verify)");
+    //clean key
+    $key=clean($_COOKIE["key"]);
     //check key
-    $check=mysql_query('select count(cID) from session where cid like "'.$_COOKIE["key"].'"');
-    $num = mysql_result ($check,0);
-
-
-
-    if ($num>0){
-      $login=true;
-      $extend=time()+600;
-      $new=mysql_query('update session set void='.$extend.' where cid like "'.$_COOKIE["key"].'"') or die ("cant update session");
-      setcookie('key',$_COOKIE["key"],$extend);
-    }
-    else
+    if(strlen($key)>=256){
+      $check=mysql_query('select count(cID) from session where cid like "'.$key.'"');
+      $num = mysql_result ($check,0);
+      if ($num>0){
+        $login=true;
+        $extend=time()+600;
+        $new=mysql_query('update session set void='.$extend.' where cid like "'.$key.'"') or die ("cant update session");
+        setcookie('key',$key,$extend);
+      }
+      else
         $login=false;
+    }
+    else{
+      $login=false;
+      '<div class="container theme-showcase" role="main"><div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-exclamation-sign"></span>&emsp;Sitzungsschlüssel defekt.<br />Bitte neu einloggen</div></div>';
+    }
     mysql_close();
     return $login;
   }
@@ -50,9 +64,10 @@ class auth {
 
     $setdb=mysql_query('use schuletest');
     if (!$setdb) {
-      die ('<br><div class="container theme-showcase" role="main"><div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-exclamation-sign"></span>&emsp;'.$sqldberror.'</div></div>');
+      die ('<br><div class="container theme-showcase" role="main"><div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-exclamation-sign"></span>&emsp;'.$sqldberror.'(logon)</div></div>');
     }
-    $loginfo=mysql_query('select password from users where uname like "'.$user.'"') or die ("Datenbankproblem oder user existiert nicht, bitte zurueck");
+    $user=clean($user);
+    $loginfo=mysql_query('select password from users where uname like "'.$user.'"') or die ("Datenbankproblem oder user existiert nicht, bitte zurück (logon)");
     $loginfo=mysql_fetch_row($loginfo)[0];
     mysql_close();
     if($pw==$loginfo)
