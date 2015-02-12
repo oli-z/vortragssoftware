d32e323c16c903b0186445f112ac62d2e4620fc6
My1 <teamhydro55555@gmail.com>
2015-02-12 18:22:33 +0100

    standardinfos hinzugefügt

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 class config{
 // MySQL-Einstellungen
-public static $db_server = ""; // DB-Server
-public static $db_user = ""; // DB-Nutzername
-public static $db_pass = ""; // DB-Passwort
-public static $db_name = ""; // DB-Name
-
+public static $dbhost = "localhost"; // DB-Server
+public static $dbuser = "schuletest"; // DB-Nutzername
+public static $dbpass = ""; // DB-Passwort
+public static $dbname = "schuletest"; // DB-Name
 // allgemeine Einstellungen
 public static $lang = "de"; // Sprache - Sprachdatei muss im "lang"-Ordner liegen
+}
 ?>
