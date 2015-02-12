a353c1abc8c663c101311540845c36f3c20bdbc1
My1 <teamhydro55555@gmail.com>
2015-02-12 16:51:38 +0100

    Update config.php
    reformat als klasse

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
-
+class config{
 // MySQL-Einstellungen
-$db_server = ""; // DB-Server
-$db_user = ""; // DB-Nutzername
-$db_pass = ""; // DB-Passwort
-$db_name = ""; // DB-Name
+public static $db_server = ""; // DB-Server
+public static $db_user = ""; // DB-Nutzername
+public static $db_pass = ""; // DB-Passwort
+public static $db_name = ""; // DB-Name
 
 // allgemeine Einstellungen
-$lang = "de"; // Sprache - Sprachdatei muss im "lang"-Ordner liegen
+public static $lang = "de"; // Sprache - Sprachdatei muss im "lang"-Ordner liegen
 ?>
