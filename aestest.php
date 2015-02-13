1a0cb647a21ef024123b1d9ce897f440d942fecd
My1 <teamhydro55555@gmail.com>
2015-02-13 22:56:45 +0100

    Update aestest.php
    neu erstellt gefixt etc.

--- aestest.php
+++ aestest.php
@@ -3,9 +3,18 @@
 <input type="submit" value="absenden" name="butooooon" class="btn btn-lg btn-success btn-block">
 </form>
 <?php
-echo($_POST['eingabedings']);
+if(isset($_POST['eingabedings'])){
+echo($_POST['eingabedings']."<br />");
 include('auth.php');
 $randkey=auth::createRandomKey();
-echo($randkey)
-auth::aescrypt($string, $randkey);
+echo($randkey."<br />");
+$aes=auth::aescrypt($_POST['eingabedings'], $randkey);
+echo $aes."<br />";
+$randkey2=auth::createRandomKey();
+echo($randkey2."<br />");
+$aes2=auth::aesdecrypt($aes, $randkey2);
+echo $aes2."<br />";
+$aes3=auth::aesdecrypt($aes, $randkey);
+echo $aes3."<br />";
+}
 ?>
