5c8974b373321d5ddcf7069b52dd85a9c3f5c886
My1 <teamhydro55555@gmail.com>
2015-02-17 23:46:30 +0100

    Anpassngen
    fixes und dergleiche

--- cuser.php
+++ cuser.php
@@ -1,13 +1,13 @@
-<form method="post" action="wpass.php" class="form-signin">
+<form method="post" action="" class="form-signin">
 <input type="text" class="form-control" name="uname" placeholder="username">
 <input type="password" class="form-control" name="pass" placeholder="pass">
 <input type="text" class="form-control" name="otp" placeholder="OTP">
-<label for="check1"> <input type="checkbox" name="admin" value="admselect" id="check1"> Admin </label>
+<label for="check1"> <input class="form-control" type="checkbox" name="admin" id="check1"> Admin </label>
 <input type="submit" value="absenden" name="butooooon" class="btn btn-lg btn-success btn-block">
 </form>
 <?php
 if(isset($_POST['uname'])&&isset($_POST["pass"])){
 include 'auth.php';
-auth::cuser($_POST['uname'],$_POST['pass']);
+auth::cuser($_POST['uname'],$_POST['pass'],isset($_POST["admin"]),$_POST["otp"]);
 }
 ?>
