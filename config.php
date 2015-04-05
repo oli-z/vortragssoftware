adff596909143165b5f88c62027a1298e2aa2f39
My1 <teamhydro55555@gmail.com>
2015-04-05 17:13:30 +0200

    Version 8

--- config.php
+++ config.php
@@ -2,11 +2,14 @@
 class config{
 // MySQL-Einstellungen
 public static $dbhost = "localhost"; // DB-Server
-public static $dbuser = "schuletest"; // DB-Nutzername
-public static $dbpass = ""; // DB-Passwort
+public static $dbuser = "schuletest"; // DB-User
+public static $dbpass = ""; // DB-Password
 public static $dbname = "schuletest"; // DB-Name
 // allgemeine Einstellungen
 public static $lang = "de"; // Sprache - Sprachdatei muss im "lang"-Ordner liegen
-public static $cpub = "6Lfzct4SAAAAAMpwqKGnuyvPitY2QWcE101EfwX_"; //Captcha-sitekey
+public static $cappub = "6Lfzct4SAAAAAMpwqKGnuyvPitY2QWcE101EfwX_"; //reCaptcha-sitekey -> leave empty to disable
+public static $capkey = "6Lfzct4SAAAAAEikbCIE7xMupWfEolC_VeMsTTwu"; //reCaptcha-secretkey -> leave empty to disable
+public static $clfpub = ''; //clef public key -> leave empty to disable
+public static $clfkey = ''; //clef private key -> leave empty to disable
 }
 ?>
