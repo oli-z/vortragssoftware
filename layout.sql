716b5a8fb8567e35cf43dcb42a8f07504839f136
My1 <teamhydro55555@gmail.com>
2015-04-13 20:35:19 +0200

    v9 prep

--- layout.sql
+++ layout.sql
@@ -7,15 +7,19 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 
+-- --------------------------------------------------------
+
+-- Auth Framework
+
 CREATE TABLE IF NOT EXISTS `session` (
 `sid` int(11) NOT NULL,
-  `type` varchar(20) NOT NULL,
+  `type` varchar(25) NOT NULL,
   `suid` int(11) NOT NULL,
   `cid` text NOT NULL,
   `void` int(11) NOT NULL
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT ;
 
-CREATE TABLE IF NOT EXISTS `users` (
+REATE TABLE IF NOT EXISTS `users` (
 `uid` int(11) NOT NULL,
   `uname` varchar(25) NOT NULL,
   `password` char(128) NOT NULL,
@@ -23,16 +27,60 @@ CREATE TABLE IF NOT EXISTS `users` (
   `admin` int(11) NOT NULL,
   `usecret` text NOT NULL,
   `clid` bigint(20) DEFAULT NULL
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT ;
 
+ALTER TABLE `session`
+MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `session`
  ADD PRIMARY KEY (`sid`);
 
 ALTER TABLE `users`
+MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `users`
  ADD PRIMARY KEY (`uid`), ADD UNIQUE KEY `uname` (`uname`), ADD UNIQUE KEY `clid` (`clid`);
 
- 
+-- -------------------------------------------------------
+
+-- Vortragsprojekt
+
+CREATE TABLE IF NOT EXISTS `slots` (
+`slid` int(11) NOT NULL,
+  `sltime` varchar(50) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT ;
+
+ALTER TABLE `slots`
+ ADD PRIMARY KEY (`slid`);
+
+ALTER TABLE `slots`
+MODIFY `slid` int(11) NOT NULL AUTO_INCREMENT;
+
+CREATE TABLE IF NOT EXISTS `vcon` (
+`coid` int(11) NOT NULL,
+  `couid` int(11) NOT NULL,
+  `covid` int(11) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT ;
+
+ALTER TABLE `vcon`
+ ADD PRIMARY KEY (`coid`);
+
+ALTER TABLE `vcon`
+MODIFY `coid` int(11) NOT NULL AUTO_INCREMENT;
+
+CREATE TABLE IF NOT EXISTS `vortrag` (
+`vid` int(11) NOT NULL,
+  `vname` varchar(100) NOT NULL,
+  `vslid` int(11) NOT NULL,
+  `limit` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT ;
+
+ALTER TABLE `vortrag`
+ ADD PRIMARY KEY (`vid`);
+
+ALTER TABLE `vortrag`
+MODIFY `vid` int(11) NOT NULL AUTO_INCREMENT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
