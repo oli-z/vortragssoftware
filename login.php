bfbc73e03ff0e533553faf69fc083fbab0ca8928
My1 <teamhydro55555@gmail.com>
2015-04-18 22:38:07 +0200

    Update login.php

--- login.php
+++ login.php
@@ -2,46 +2,69 @@
 require_once('auth.php');
 require_once('config.php');
 require_once('lang/'.config::$lang.'.php');
-$logincomplete=false;
-if(isset($_POST["kill"])) {
+$user=false;
+@session_start();
+if(isset($_SESSION["msg"]))
+  auth::$msg.=$_SESSION["msg"];
+session_destroy();
+//auth::$msg.=style::warn("lol").style::info("lol").style::success("lol").style::error("lol");  //WISE-debug
+ if(isset($_POST["ghostout"])&&$_POST["ghostout"]) {
+  auth::logout("ghost",auth::verify());
+}
+if(isset($_POST["kill"])&&$_POST["kill"]) {
   auth::logout();
 }
 if(!isset($_POST['g-recaptcha-response']))
   $_POST['g-recaptcha-response']="";
-if (isset($_POST["sub"])&&$_POST["pass"]&&$_POST["user"]) {
-  $logincomplete = auth::logon($_POST["user"],$_POST["pass"],$_POST["otp"],$_POST['g-recaptcha-response']);
+if(!isset($_POST['otp']))
+  $_POST['otp']="";
+if (isset($_POST["sub"])/*&&$_POST["pass"]&&$_POST["user"]*/) {
+  auth::logon($_POST["user"],$_POST["pass"],$_POST["otp"],$_POST['g-recaptcha-response']);
 }
 else {
-  $vid=auth::verify();
+  $user=auth::verify();
 }
-echo ('
-  <html lang="de">
+if(isset($_POST["clfdis"])&&$_POST["clfdis"]) {
+  auth::clfdis($user);
+}
+if(!empty(auth::$msg))
+  auth::$msg.="<br>";
+  echo('<html lang="de">
     <head>
       <meta http-equiv="content-type" content="text/html; charset=utf-8">
       <link rel="stylesheet" href="inc/css/bootstrap.min.css"> <!-- Bootstrap CSS laden -->
+      <link rel="stylesheet" href="inc/css/bootstrap-theme.min.css"> <!-- Bootstrap CSS laden -->
+      <link rel="stylesheet" href="inc/css/fa.css"> <!-- FontAwesome CSS laden -->
+      <noscript><style>
+      .js {
+        display:none;
+      }
+      </style></noscript>
     </head>
     <body>
       <div class="container">
       <br>
       <br>
-      <div class="jumbotron">
+      <div class="jumbotron">'.auth::$msg);
+echo ('
       <form method="post" action="login.php" class="form-signin">');
-if(!isset($vid))
-  $vid=false;
+if(!isset($user))
+  $user=false;
 if(isset($_POST["user"])&&$_POST["user"])
   $val=$_POST["user"];
 else
   $val="";
-if(!((ctype_digit($vid)&&$vid)||$logincomplete)) {
+if(!((ctype_digit($user)&&$user)||$user)) {
   echo('
-  <input type="text" class="form-control" name="user" placeholder="Banutzername" value="'.$val.'" style="margin-left: auto;margin-right: auto;width:50%;">
+  <input type="text" class="form-control" name="user" placeholder="'.lang::$user.'" value="'.$val.'" style="margin-left: auto;margin-right: auto;width:50%;">
   <br>
-  <input type="password" class="form-control" name="pass" placeholder="passwort" style="margin-left: auto;margin-right: auto;width:50%;">
-  <br>
-  <input type="text" maxlength="6" class="form-control" name="otp" placeholder="OTP, wenn du nicht weißt was das ist, leerlassen" autocomplete="off" style="margin-left: auto;margin-right: auto;width:50%;">
-  <br>');
-if(isset(config::$cappub)&&config::$cappub&&isset(config::$capkey)&&config::$capkey)
-  echo ('<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+  <input type="password" class="form-control" name="pass" placeholder="'.lang::$pass.'" style="margin-left: auto;margin-right: auto;width:50%;">');
+  if(config::$otpact)
+    echo ('<br>
+  <input type="text" maxlength="6" class="form-control" name="otp" placeholder="'.lang::$otp.'" autocomplete="off" style="margin-left: auto;margin-right: auto;width:50%;">');
+if(isset(config::$capact)&&config::$capact&&isset(config::$cappub)&&config::$cappub&&isset(config::$capkey)&&config::$capkey)
+  echo ('<br>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script> <!-- reCaptcha -->
     <div class="g-recaptcha" data-sitekey="'.config::$cappub.'" style="margin-left: auto;margin-right: auto; display: table;"></div>
     <noscript>
       <div style="width: 302px; height: 352px; margin-left: auto;margin-right: auto;">
@@ -63,23 +86,31 @@ if(isset(config::$cappub)&&config::$cappub&&isset(config::$capkey)&&config::$cap
       </div>
     </noscript>');
 echo('<br>
-<input type="submit" value="absenden" name="sub" class="btn btn-lg btn-success btn-block" style="margin-left: auto;margin-right: auto;width:25%;"><br>');
-  if(isset(config::$clfpub)&&config::$clfpub)
-  echo ('<div style="margin-left:auto; margin-right:auto; display: table; font-size: 2em;">ODER<br></div>
+<input type="submit" value="'.lang::$login.'" name="sub" class="btn btn-lg btn-success btn-block" style="margin-left: auto;margin-right: auto;width:25%;">');
+  if(isset(config::$clfact)&&config::$clfact&&isset(config::$clfpub)&&config::$clfpub&&isset(config::$clfkey)&&config::$clfkey)
+  echo ('<span class="js"><br><div style="margin-left:auto; margin-right:auto; display: table; font-size: 2em;">ODER</div><br>
     <div class="clef-wrapper" style="margin-left:auto; margin-right:auto; width:188px;">
       <script data-type="login" data-redirect-url="'.config::$clfred.'"  data-style="button" data-color="blue" data-app-id="'.config::$clfpub.'"  class="clef-button" src="https://clef.io/v3/clef.js" type="text/javascript"></script>
-    </div>');
+    </div></span>');
 }
 else {
-  if(!$vid)
-    $vid=$logincomplete;
-  echo('<input type="submit" value="ausloggen" name="kill" class="btn btn-lg btn-warning btn-block" style="margin-left: auto;margin-right: auto;width:25%;">');
-}
-
-if(ctype_digit($vid)&&$vid){
-  echo("<div>login OK hier gehts weiter</div><br>".$vid."(verify)<br>"/* -> debug*/);
-  if(auth::isadmin($vid)) echo "admin";
+  echo('<input type="submit" value="'.lang::$logout.'" name="kill" class="btn btn-lg btn-warning btn-block" style="margin-left: auto;margin-right: auto;width:25%;">');
+  echo('<input type="submit" value="'.lang::$ghost.'" name="ghostout" class="btn btn-lg btn-danger btn-block" style="margin-left: auto;margin-right: auto;width:25%;">
+  login OK <a href="choose.php">hier</a> gehts weiter<br>user ID:'.$user.'<br>'/* -> debug*/);
+  if(auth::isadmin($user))
+    echo 'zum adminpanel <a href="admin/index.php">hier</a> lang';
   //echo($_SERVER['HTTP_USER_AGENT']); //debug
+  if(config::$clfact&&auth::getcdata($user,"clid")) {
+    if(auth::verify("type")!="clef")
+      echo '<br>Dein Account ist mit der Clef-ID '.auth::getcdata($user,"clid").' verbunden.
+  zum trennen klicke bitte <input type="submit" value="hier" name="clfdis" class="btn btn-xs btn-warning">';
+    else
+      echo '<br>um deine clef-ID zu trennen, logge dich bitte mit Benutzername und Passwort ein.';
+  }
+  else {
+    echo ('<span class="js"><br>Wenn du deinen Account mit clef verbinden möchtest, klicke <a class="clef-button btn btn-xs btn-info" data-app-id="'.config::$clfpub.'" data-color="blue" data-style="flat" data-redirect-url="'.config::$clfred.'" data-custom="true">hier</a></span>
+    <script type="text/javascript" src="https://clef.io/v3/clef.js"></script>');
+  }
 }
 echo('</form>
       </div>
