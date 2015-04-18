7b2f92b60e570b0a0896e23a4187dce5d0880e24
My1 <teamhydro55555@gmail.com>
2015-04-18 22:50:46 +0200

    Update clef.php

--- clef.php
+++ clef.php
@@ -1,11 +1,14 @@
 <?php
 include ("auth.php");
 if (isset($_GET["type"])&&$_GET["type"]=="clef"&&isset($_GET["code"])) {
-  auth::logon(0,0,0,0,$_GET["code"]);
+  if(!auth::logon(0,0,0,0,$_GET["code"])) {
+    session_start();
+    $_SESSION["msg"].=auth::$msg;
+  }
   header("Location: login.php");
 }
 
 if (isset($_GET["type"])&&$_GET["type"]=="clefout"&&isset($_POST['logout_token'])) {
-  auth::clefout($_POST['logout_token']);
+  auth::logout("clef",$_POST['logout_token']);
 }
 ?>
